36e91388565c83add0022c55e0789872c172426a
Guðmundur Óli <gudmundurha203@nemi.tskoli.is>
2018-12-10 13:05:48 +0000

    Add files via upload

--- Verkefni3.sql
+++ Verkefni3.sql
@@ -1,7 +1,5 @@
 use 1404002030_progresstracker_v1;
 
-# Gömul útgáfa.  Gat ekki náð í nýja því þetta tækniskólaský virkar eiginlega aldrei.  Mun skipta á mánudaginn
-
 # 1
 
 delimiter &&
@@ -9,17 +7,12 @@ delimiter &&
 drop procedure if exists SemesterInfo &&
 
 create procedure SemesterInfo()
-begin
-/*
-	select JSON_OBJECTAGG(
-			"Nafn", studentName
-            "DOB", "",
-            "Áfangar", select JSON_OBJECTAGG(
-				"Áfangi", CONCAT(courseNumber)
-            ) as Áfangar from courses
-    ) as Nemendur from students;
-    */
-    select * from students;
+begin    
+    select group_concat("{", 
+    (select concat('"nafn": "', studentName, '",', '"DOB": "7. Janúar. 1997', '", "Áfangar": [', (select group_concat('"', courseNumber, '", ') from trackcourses where trackcourses.trackID = Students1.trackID), ']')
+    from students where students.studentID = Students1.studentID)
+    ,"}") as JsonData from students as Students1;
+    
 end &&
 
 
@@ -28,4 +21,25 @@ delimiter ;
 
 call SemesterInfo();
 
-select version();
+# 2
+
+alter table schools add schoolInfo json;
+
+update schools set schoolInfo = JSON_OBJECT("name", "Tækniskólinn", "addresstype", "multiple", "address", JSON_ARRAY("Skólavörðuholti(Aðalbygging)", "Skólavörðuholt", "Flatahrauni", "Háteigsvegi"), "cost", "20200" ) where schoolID = 1;
+
+insert into schools (schoolName, schoolInfo) values ("Borgarholtsskóli", JSON_OBJECT("name", "Borgarholtsskóli", "addresstype", "single", "address", "Mosavegi", "cost", "12600"));
+
+insert into schools (schoolName, schoolInfo) values ("Fjölbrautaskólinn við Ármúla", JSON_OBJECT("name", "Fjölbrautaskólinn við ármúla", "addresstype", "single", "address", "Ármúla 12", "cost", "18000"));
+
+delimiter &&
+
+drop procedure if exists GetAllSchoolInfo &&
+
+create procedure GetAllSchoolInfo(id int(10))
+begin    
+    select schoolInfo from schools where schoolID = id;
+end &&
+
+delimiter ;
+
+call GetAllSchoolInfo(4);
